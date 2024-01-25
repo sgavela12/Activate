0073d762d444b459b0a6254f0e36d4e968874187
sgavela12 <sgavela12@gmail.com>
2024-01-25 11:31:29 +0100

    añadida la comprobacion en el formulario de registro para que compruebe que la contraseña y el confirmar contraseña son iguales, cuando lo son lo envia

--- src/main/resources/static/js/registrarse.js
+++ src/main/resources/static/js/registrarse.js
@@ -1,13 +1,71 @@
+let registrarse = document.getElementById("registrarse")
+
 document.addEventListener('DOMContentLoaded', function () {
-    var loginTab = document.getElementById('loginTab');
-    var signupTab = document.getElementById('signupTab');
-    var cardWrapper = document.querySelector('.card-3d-wrapper');
+let loginTab = document.getElementById('loginTab');
+let signupTab = document.getElementById('signupTab');
+let regLogCheckbox = document.getElementById('reg-log');
+let cardWrapper = document.querySelector('.card-3d-wrapper');
+let arrowLabel = document.querySelector('.arrow-label');
 
-    loginTab.addEventListener('click', function () {
-      cardWrapper.style.transform = 'rotateY(0deg)';
-    });
+function showLoginForm() {
+cardWrapper.style.transform = 'rotateY(0deg)';
 
-    signupTab.addEventListener('click', function () {
-      cardWrapper.style.transform = 'rotateY(180deg)';
-    });
+}
+
+function showSignupForm() {
+cardWrapper.style.transform = 'rotateY(180deg)';
+
+}
+
+loginTab.addEventListener('click', function () {
+showLoginForm();
+regLogCheckbox.checked = false;
+});
+
+signupTab.addEventListener('click', function () {
+showSignupForm();
+regLogCheckbox.checked = true;
+});
+
+regLogCheckbox.addEventListener('change', function () {
+if (regLogCheckbox.checked) {
+  showSignupForm();
+} else {
+  showLoginForm();
+}
+});
+});
+
+registrarse.addEventListener("click",comprobarClave)
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  let formRegistro = document.getElementById('formulario-registro');
+
+  formRegistro.addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      comprobarClave();
+
+      if (validacionExitosa) {
+          this.submit();
+      }
   });
+});
+
+function comprobarClave() {
+  let clave1 = document.getElementById("contraseña").value;
+  let clave2 = document.getElementById("confirmar-contraseña").value;
+
+  if (clave1 === clave2) {
+      alert("Las dos claves son iguales...\nRealizaríamos las acciones del caso positivo");
+
+      validacionExitosa = true;
+  } else {
+      alert("Las dos claves son distintas...\nRealizaríamos las acciones del caso negativo");
+      validacionExitosa = false;
+  }
+}
+
