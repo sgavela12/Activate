a205fcd3680a4e0abf59ce3b36d9710f5b3423ac
sgavela12 <sgavela12@gmail.com>
2024-05-28 19:12:28 +0200

    Comento cambios para que no de error al crear mas usuarios

--- src/main/java/com/example/activate/ActivateApplication.java
+++ src/main/java/com/example/activate/ActivateApplication.java
@@ -18,13 +18,13 @@ public class ActivateApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ActivateApplication.class, args);
 	}
-	  @Bean
-    public CommandLineRunner agregarUsuario(UsuarioService usuarioService) {
-        return args -> {
-            // Añadir un nuevo usuario al inicio de la aplicación
-            usuarioService.añadir(new Usuario("admin", Rol.ADMIN, "1234",LocalDate.of(1990, Month.JANUARY, 1)));
-        };
+	//   @Bean
+    // public CommandLineRunner agregarUsuario(UsuarioService usuarioService) {
+    //     return args -> {
+    //         // Añadir un nuevo usuario al inicio de la aplicación
+    //         usuarioService.añadir(new Usuario("admin", Rol.ADMIN, "1234",LocalDate.of(1990, Month.JANUARY, 1)));
+    //     };
 	
-	}
+	// }
 }
 	
