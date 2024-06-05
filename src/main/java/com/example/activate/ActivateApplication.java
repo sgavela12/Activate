5ae4f710293b9eb0e519a1c7462da8bd5aa7a193
sgavela12 <sgavela12@gmail.com>
2024-06-05 21:03:05 +0200

    adicion de sentencias y dietas

--- src/main/java/com/example/activate/ActivateApplication.java
+++ src/main/java/com/example/activate/ActivateApplication.java
@@ -1,7 +1,16 @@
 package com.example.activate;
 
+import java.time.LocalDate;
+import java.time.Month;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.activate.models.Rol;
+import com.example.activate.models.Usuario;
+import com.example.activate.service.UsuarioService;
 
 
 
